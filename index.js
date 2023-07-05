2a7a383223e36bb73b480e385a079464173856f2
Xaveer van der Laan <xavlaan@protonmail.com>
2023-07-05 22:23:47 +0200

    Added start log message

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import * as fs from 'node:fs';
 
 const folderPath = './scripts';
 
+console.log('Starting this party!');
+
 fs.access(folderPath, fs.constants.F_OK, () => {
     console.log('Folder accesible!')
 });
